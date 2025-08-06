ba74f0919311d89ac85e3023f7088719c454146b
Jivesh423 <jivusorout@gmail.com>
2025-08-06 15:41:58 +0530

    Added sub() method

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,8 @@
 public class Calculator {
-    // Methods will be added in feature branches
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
 }
